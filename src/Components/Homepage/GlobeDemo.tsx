d788b4aaa21c90b0240b8e01677e698fb880770d
Gauravbvishal <102813879+Gauravbvishal@users.noreply.github.com>
2024-10-23 11:28:56 +0530

    Your commit message here

--- src/Components/Homepage/GlobeDemo.tsx
+++ src/Components/Homepage/GlobeDemo.tsx
@@ -1,6 +1,5 @@
-"use client";
+ "use client";
 import React from "react";
-import { motion } from "framer-motion";
 import dynamic from "next/dynamic";
 import { sampleArcs } from "@/data/Samplearcs";
 
@@ -23,23 +22,21 @@ export function GlobeDemo() {
     directionalLeftLight: "#ffffff",
     directionalTopLight: "#ffffff",
     pointLight: "#ffffff",
-    arcTime: 1000,
+    arcTime: 500,
     arcLength: 0.9,
     rings: 1,
     maxRings: 3,
     initialPosition: { lat: 22.3193, lng: 114.1694 },
     autoRotate: true,
-    autoRotateSpeed: 0.5,
+    autoRotateSpeed: 0.2,
   };
-  
-  
 
   return (
-    
-          <div className="w-full h-[60vh] ">
-            <World data={sampleArcs} globeConfig={globeConfig} />
-          </div>
-        
+    <div className="flex justify-center items-center w-full h-screen">
+      {/* Adjusting the responsive globe container for iPad Pro */}
+      <div className="w-full max-w-[90vw] h-[50vh] sm:h-[65vh] md:h-[70vh] lg:h-[75vh] xl:h-[80vh]">
+        <World data={sampleArcs} globeConfig={globeConfig} />
+      </div>
+    </div>
   );
-  
 }
